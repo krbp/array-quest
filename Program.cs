71d70f4d634db80f29d5e605753e6b9ae75b1987
krbp <165799825+krbp@users.noreply.github.com>
2024-04-08 06:53:27 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] arrA = { 1, 2 };
+        int[] arrB = { 3, 4 };
+        int[] arrC = { arrA[0], arrA[1], arrB[0], arrB[1] };
+    }
+}
